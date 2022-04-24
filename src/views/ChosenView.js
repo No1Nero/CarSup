590b7a1bad077f46c42519b859f17f1129af1e0a
No1Nero <nikita.pliutenko56@gmail.com>
2022-04-24 20:21:12 +0300

    end design

--- src/views/ChosenView.js
+++ src/views/ChosenView.js
@@ -1,12 +1,10 @@
 import React from "react";
-import CarList from "components/CarList/CarList";
 import s from './ChosenView.module.css';
 
 export default function ChosenView() {
     return (
-        <div>
-            <h1 className={s.header}>Избранное</h1>
-            <CarList nameLink={'/chosen/'}/>
+        <div className={s.wrapper}>
+
         </div>
     );
 };
